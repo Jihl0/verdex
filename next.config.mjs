220705ddcb98e0c02a08d2bed1ab39c54b5ff381
Jihl0 <jralviar4@up.edu.ph>
2025-07-17 16:21:36 +0800

    fix Deployment error 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // Enable if using Firebase Storage/Realtime DB
   images: {
-    domains: ["firebasestorage.googleapis.com"], // Add other domains if needed
+    domains: ["firebasestorage.googleapis.com"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
